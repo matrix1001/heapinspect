7d3f1d02d85e15c7ec14e8fce8656bedc6687f05
matrix1001 <simplematrix1001@gmail.com>
2018-10-22 00:06:52 -0400

    c_struct in use

--- test.py
+++ test.py
@@ -14,53 +14,161 @@ def p64(i):
 def p32(i):
     return struct.pack('<I', i)
 
-class malloc_state(object):
-    def __init__(self, memdump, arch='64', version='2.27'):
-        self.memdump = memdump
-        if version == '2.27' and arch == '64':
-            assert len(memdump) >= 0x898
-            self.mutex = u32(memdump[0:4])
-            self.flags = u32(memdump[4:8])
-            self.have_fastchunks = u32(memdump[8:12])
-            self.fastbinsY = []
-            for i in range(10):
-                self.fastbinsY.append(u64(memdump[16+8*i:24+8*i]))
-            self.top = u64(memdump[0x60:0x68])
-            self.last_remainder = u64(memdump[0x68:0x70])
-            self.bins = []
-            for i in range(254):
-                self.bins.append(u64(memdump[0x70+i*8:0x78+i*8]))
-            self.binmap = []
-            for i in range(4):
-                self.binmap.append(u64(memdump[0x860+i*4:0x864+i*4]))
-            self.next = u64(memdump[0x870:0x878])
-            self.next_free = u64(memdump[0x878:0x880])
-            self.attached_threads = u64(memdump[0x880:0x888])
-            self.system_mem = u64(memdump[0x888:0x890])
-            self.max_system_mem = u64(memdump[0x890:0x898])
-
-class malloc_chunk(object):
-    def __init__(self, memdump, arch='64'):
-        self.memdump = memdump
+malloc_state_struct = '''
+struct malloc_state
+{
+    int mutex;
+    int flags;
+    int have_fastchunks;
+    int align;
+    ptr fastbinsY[10];
+    ptr top;
+    ptr last_remainder;
+    ptr bins[254];
+    int binmap[4];
+    ptr next;
+    ptr next_free;
+    size_t attached_threads;
+    size_t system_mem;
+    size_t max_system_mem;
+}
+'''
+malloc_chunk_struct = '''
+struct malloc_chunk
+{
+    size_t prev_size;
+    size_t size;
+    ptr fd;
+    ptr bk;
+}
+'''
+class C_Struct(object):
+    def __init__(self, code, arch='64', endian='little'):
         if arch == '64':
-            assert len(memdump) >= 0x20
-            self.prev_size = u64(memdump[0:8])
-            self.size = u64(memdump[8:16])
-            self.fd = u64(memdump[16:24])
-            self.bk = u64(memdump[24:32])
-            #self.fd_nextsize = u64(memdump[32:40])
-            #self.bk_nextsize = u64(memdump[40:48])
+            self._typ2size = {
+                'bool':1,
+                'byte':1,
+                'char':1,
+                'int':4,
+                'ptr':8,
+                'size_t':8
+            }
+        elif arch == '32':
+            self._typ2size = {
+                'bool':1,
+                'byte':1,
+                'char':1,
+                'int':4,
+                'ptr':4,
+                'size_t':4
+            }
+        else:
+            raise NotImplementedError("Not supported arch for C_Struct")
+        self._arch = arch
+        self._endian = endian
+        self._code = code
+        self._struct_name = re.search('^\s*struct\s+(\w+)\s*{', code).groups()[0]
+        self._vars = []
+        for v in re.findall('\s*(\w*)\ (\w*)\[?(\d+)?\]?;', code):
+            typ, name, num = v
+            if num == '': num = int(1)
+            else: num = int(num)
+            self._vars.append((typ, name, num))
+    
+        self._dict = {}
+        for v in self._vars:
+            typ, name, num = v
+            self._dict[name] = {"typ":typ, "memdump":None, "num":num}
 
-class tcache_perthread_struct(object):
-    def __init__(self, memdump, arch='64'):
-        self.memdump = memdump
-        self.counts = []
-        self.entries = []
-        if arch == '64':
-            assert len(memdump) >= 0x240
-            for i in range(64):
-                self.counts.append(ord(memdump[i]))
-                self.entries.append(ord(memdump[i*8+64]))
+        self._addr = 0
+        self._mem = None
+
+    @property
+    def _size(self):
+        size = 0
+        for v in self._vars:
+            typ, name, num = v
+            
+            size += self._typ2size[typ] * num
+        return size
+
+    def _offset(self, var):
+        offset = 0
+        var_name, var_index = re.findall('^(\w*)\[?(\d+)?\]?$', var)[0]
+        if var_index == '': var_index = 0
+        else: var_index = int(var_index)
+        for v in self._vars:
+            typ, name, num = v
+            if name == var_name:
+                offset += var_index * self._typ2size[typ]
+                break
+            
+            offset += self._typ2size[typ] * num
+        return offset
+
+    def _addrof(self, var):
+        return self._addr + self._offset(var)
+
+    def _sizeof(self, var):
+        var_name, var_index = re.findall('^(\w*)\[?(\d+)?\]?$', var)[0]
+        typ = self._dict[var_name]['typ']
+        num = self._dict[var_name]['num']
+
+        if var_index == '':  # get total size
+            return self._typ2size[typ] * num
+        else:
+            return self._typ2size[typ] # get one size
+    
+    def _init(self, memdump, addr = 0):
+        #assert len(memdump) >= self.size
+        if len(memdump) < self._size:
+            memdump.ljust(self._size, '\0')
+        for v in self._vars:
+            typ, name, num = v
+            offset = self._offset(name)
+            size = self._sizeof(name)
+            self._dict[name]['memdump'] = memdump[offset:offset+size]
+
+        self._mem = memdump
+        self._addr = addr
+
+    def _new(self, memdump, addr = 0):
+        new_obj = C_Struct(self._code, self._arch, self._endian)
+        new_obj._init(memdump, addr)
+
+        return new_obj
+    def __getattr__(self, var_name):
+        
+        if var_name in self._dict:
+            typ = self._dict[var_name]['typ']
+            num = self._dict[var_name]['num']
+            memdump = self._dict[var_name]['memdump']
+
+            a_size = self._typ2size[typ]
+
+            unpack = lambda x:x
+            if typ == 'int':
+                unpack = lambda x:u32(x)
+            elif (typ == 'size_t' or typ == 'ptr') and self._arch == '32':
+                unpack = lambda x:u32(x)
+            elif (typ == 'size_t' or typ == 'ptr') and self._arch == '64':
+                unpack = lambda x:u64(x)
+            
+
+            if num > 1:
+                result = []
+                for i in range(num):
+                    mem = memdump[i*a_size:i*a_size+a_size]
+                    result.append(unpack(mem))
+                return result
+            else:
+                return unpack(memdump)
+        else:
+            return None
+            
+
+MallocState = C_Struct(malloc_state_struct)
+MallocChunk = C_Struct(malloc_chunk_struct)
 
 class HeapInspector(object):
     def __init__(self, pid):
@@ -89,18 +197,22 @@ class HeapInspector(object):
 
     @property
     def main_arena(self):
-        return malloc_state(self.arenamem)
+        libc_base = self.proc.bases['libc']
+        arena_addr = libc_base + self.libc_info['main_arena_offset']
+        return MallocState._new(self.arenamem, arena_addr)
 
     @property
     def heap_chunks(self):
         heap_mem = self.heapmem
+        heap_base = self.proc.bases['heap']
         cur_pos = 0
         result = []
         while cur_pos < len(heap_mem):
             cur_block_size = u64(heap_mem[cur_pos+0x8:cur_pos+0x10]) & ~0b111
             memblock = heap_mem[cur_pos:cur_pos+cur_block_size]
+            result.append(MallocChunk._new(memblock, cur_pos + heap_base))
             cur_pos = (cur_pos+cur_block_size) & ~0b1111
-            result.append(malloc_chunk(memblock))
+            
 
         return result
 
@@ -113,7 +225,7 @@ class HeapInspector(object):
             while fastbin_ptr:
                 
                 mem = self.proc.read(fastbin_ptr, 0x20)
-                chunk = malloc_chunk(mem)
+                chunk = MallocChunk._new(mem)
                 lst.append(chunk)
                 fd = chunk.fd
                 fastbin_ptr = fd
@@ -125,145 +237,22 @@ class HeapInspector(object):
     @property
     def unsortedbins(self):
         result = []
-        unsorted_ptr = self.main_arena.last_remainder
+        unsorted_ptr = self.main_arena.bins[0]
         while unsorted_ptr:
             mem = self.proc.read(unsorted_ptr, 0x20)
-            chunk = malloc_chunk(mem)
+            chunk = MallocChunk._new(mem)
             lst.append(chunk)
             fd = chunk.fd
             bk = chunk.bk
             unsorted_ptr = fd
             
-sample = '''
-struct test
-{
-    int mutex;
-    int flags;
-    int have_fastchunks;
-    int align;
-    ptr fastbinsY[10];
-    ptr top;
-    ptr last_remainder;
-    ptr bins[254];
-    int binmap[4];
-    ptr next;
-    ptr next_free;
-    size_t attached_threads;
-    size_t system_mem;
-    size_t max_system_mem;
-}
-'''
-sample = '''
-struct malloc_chunk
-{
-    size_t prev_size;
-    size_t size;
-    ptr p[2];
-}
-'''
-class C_Struct(object):
-    def __init__(self, code, arch='64', endian='little'):
-        if arch == '64':
-            self._typ2size = {
-                'bool':1,
-                'byte':1,
-                'char':1,
-                'int':4,
-                'ptr':8,
-                'size_t':8
-            }
-        self._endian = endian
-        self._code = code
-        self._struct_name = re.search('^\s*struct\s+(\w+)\s*{', code).groups()[0]
-        self._vars = []
-        for v in re.findall('\s*(\w*)\ (\w*)\[?(\d+)?\]?;', code):
-            typ, name, num = v
-            if num == '': num = int(1)
-            else: num = int(num)
-            self._vars.append((typ, name, num))
-    
-        self._dict = {}
-        for v in self._vars:
-            typ, name, num = v
-            self._dict[name] = {"typ":typ, "memdump":None, "num":num}
-
-    @property
-    def _size(self):
-        size = 0
-        for v in self._vars:
-            typ, name, num = v
-            
-            size += self._typ2size[typ] * num
-        return size
-
-    def _offset(self, var):
-        offset = 0
-        var_name, var_index = re.findall('^(\w*)\[?(\d+)?\]?$', var)[0]
-        if var_index == '': var_index = 0
-        else: var_index = int(var_index)
-        for v in self._vars:
-            typ, name, num = v
-            if name == var_name:
-                offset += var_index * self._typ2size[typ]
-                break
-            
-            offset += self._typ2size[typ] * num
-        return offset
-
-    def _sizeof(self, var):
-        var_name, var_index = re.findall('^(\w*)\[?(\d+)?\]?$', var)[0]
-        typ = self._dict[var_name]['typ']
-        num = self._dict[var_name]['num']
-
-        if var_index == '':  # get total size
-            return self._typ2size[typ] * num
-        else:
-            return self._typ2size[typ] # get one size
-    
-    def _new(self, memdump):
-        #assert len(memdump) >= self.size
-        if len(memdump) < self._size:
-            memdump.ljust(self._size, '\0')
-        for v in self._vars:
-            typ, name, num = v
-            offset = self._offset(name)
-            size = self._sizeof(name)
-            self._dict[name]['memdump'] = memdump[offset:offset+size]
-
-        return copy.copy(self)
-    def __getattr__(self, var_name):
-        
-        typ = self._dict[var_name]['typ']
-        num = self._dict[var_name]['num']
-        memdump = self._dict[var_name]['memdump']
-
-        a_size = self._typ2size[typ]
-
-        if num > 1:
-            result = []
-            for i in range(num):
-                mem = memdump[i*a_size:i*a_size+a_size]
-                result.append(mem)
-            return result
-        else:
-            return memdump
-            
-
-a = C_Struct(sample)
-mem = p64(0x60) + p64(0x20) + p64(0xdeadbeef) + p64(0)
-b = a._new(mem)
-
-    #def __getattr__(self, attr):
-
-
-
-
 
 
 if __name__ == '__main__':
-    hi = HeapInspector(21411)
+    hi = HeapInspector(5302)
+    chunks = hi.heap_chunks
     for chunk in hi.heap_chunks:
-        print("chunk: psize={} size={} fd={} bk={}".format(hex(chunk.prev_size), hex(chunk.size), hex(chunk.fd), hex(chunk.bk)))
+        print("chunk({}): prev_size={} size={} fd={} bk={}".format(hex(chunk._addr), hex(chunk.prev_size), hex(chunk.size), hex(chunk.fd), hex(chunk.bk)))
     
     
 
